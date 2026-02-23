1278dab7760f8095e8d9de68f778a6d6f1dd0d1a
SARGIOUS <joys62478@gmail.com>
2026-02-23 18:05:49 +0530

    first commit

---
+++ src/test/hello.java
@@ -0,0 +1,21 @@
+package test;
+
+public class hello {
+
+	public static void main(String[] args) {
+
+		String str = "hello World";
+		StringBuilder sb = new StringBuilder();
+		sb.append(str);
+		System.out.println(str);
+		String res = "";
+		for(int i=0;i<str.length();i++) {
+			res=str.charAt(i)+res;
+			
+			}
+		System.out.println(res);
+	}
+		
+	}
+
+
